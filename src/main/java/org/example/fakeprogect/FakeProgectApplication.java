06a0e45602e64a480f1d9368eecef77ea480cd4d
FakeJavaLearner <miriman@ukr.net>
2024-08-19 12:39:55 +0200

    initial commit

---
+++ src/main/java/org/example/fakeprogect/FakeProgectApplication.java
@@ -0,0 +1,13 @@
+package org.example.fakeprogect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeProgectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeProgectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fakeprogect/FakeProgectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fakeprogect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeProgectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
